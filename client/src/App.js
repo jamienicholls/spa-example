73806458b56f7f2d2d249b8286aaff0a095579e4
Jamie Nicholls <J.Nicholls@massey.ac.nz>
2023-05-01 13:18:06 +1200

    Init

---
+++ client/src/App.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import './App.css';
+
+function App() {
+  const [objectR, setObjectR] = useState({});
+  const [arrayR, setArrayR] = useState({});
+  const [objectArrayR, setObjectArrayR] = useState({});
+
+  useEffect(() => {
+    const fetchObjectR = async () => {
+      const response = await fetch(
+        'http://localhost:3000/object'
+      );
+      const data = await response.json();
+      setObjectR(data);
+    };
+    fetchObjectR();
+    const fetchArrayR = async () => {
+      const response = await fetch(
+        'http://localhost:3000/array'
+      );
+      const data = await response.json();
+      setArrayR(data);
+    };
+    fetchArrayR();
+    const fetchObjectArrayR = async () => {
+      const response = await fetch(
+        'http://localhost:3000/object-array'
+      );
+      const data = await response.json();
+      setObjectArrayR(data);
+    };
+    fetchObjectArrayR();
+  }, []);
+
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <div class="mt-4 p-5 bg-primary text-white"><h1>API Example calls</h1></div>
+        </Row>
+        <Row>
+          <Col>
+            <Tabs
+              defaultActiveKey="object"
+              id="fill-tab-example"
+              className="mb-3 mt-3 justify-content-center"
+              >
+              <Tab eventKey="object" title="Form Example">
+                <Form.Group className="mb-3">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control value={objectR.title} disabled />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control value={objectR.description} disabled />
+                </Form.Group>
+
+                <div className="mb-3">
+                  <label class="form-label">Tags</label>
+                  <ul>
+                    {(Array.isArray(objectR.tags)) && objectR.tags.map((item, index) => (
+                      <li key={index} >{item}</li>
+                    ))}
+                  </ul>
+                </div>
+                <hr />
+                <small>Object: <pre>{JSON.stringify(objectR)}</pre></small>
+              </Tab>
+              <Tab eventKey="array" title="List">
+                <div className="mb-3">
+                  <label class="form-label">List</label>
+                  <ul>
+                    {(Array.isArray(arrayR)) && arrayR.map((item, index) => (
+                      <li key={index} >{item}</li>
+                    ))}
+                  </ul>
+                </div>
+                <hr />
+                <small>Array: <pre>{JSON.stringify(arrayR)}</pre></small>
+              </Tab>
+              <Tab eventKey="object-array" title="Table">
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Title</th>
+                      <th>Description</th>
+                      <th>Tags</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {(Array.isArray(objectArrayR)) && objectArrayR.map((item, index) => (
+                      <tr key={index} >
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.description}</td>
+                        <td>
+                          {(Array.isArray(item.tags)) && item.tags.map((item, index) => (
+                            <span key={index} >{item}, </span>
+                          ))}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+
+                <hr />
+                <small>Object array: <pre>{JSON.stringify(objectArrayR)}</pre></small>
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+
+export default App;
+
---
+++ server/src/server.js
@@ -0,0 +1,123 @@
+import Hapi from '@hapi/hapi';
+import Inert from '@hapi/inert';
+
+const plugins = [];
+plugins.push(Inert);
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    await server.register(plugins);
+
+    // Client
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '../client/build',
+                redirectToSlash: true,
+                index: true,
+            },
+        }
+    });
+
+    // API Routes
+    server.route({
+        method: 'GET',
+        path: '/hello-world',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/array',
+        handler: (request, h) => {
+            const array = ['one', 'two', 'three', 'four']
+            return array;
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/object',
+        handler: (request, h) => {
+            const object = {
+                title: 'Object One',
+                description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                tags: ['example', 'object', 'api']
+            }
+            return object;
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/object-array',
+        handler: (request, h) => {
+            const objectArray = [
+                {
+                    id: 1,
+                    title: 'Object One',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                },
+                {
+                    id: 2,
+                    title: 'Object Two',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                },
+                {
+                    id: 3,
+                    title: 'Object Three',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                },
+                {
+                    id: 4,
+                    title: 'Object Four',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                },
+                {
+                    id: 5,
+                    title: 'Object Five',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                },
+                {
+                    id: 6,
+                    title: 'Object Six',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                },
+                {
+                    id: 7,
+                    title: 'Object Seven',
+                    description: 'BBsadhjA Afhd jf dsjfakjsf asdjafjkhasdjfhwaerj f jkd sadfjklhajkrf asdlkjfhjawklrrwjkf fdsjfasjkdfhlaskdfasdjk',
+                    tags: ['example', 'object', 'api']
+                }
+            ];
+            return objectArray;
+        }
+    });
+
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
