07c10c090e2a54ae24e52de9d254f6486a3815d1
Jamie Nicholls <J.Nicholls@massey.ac.nz>
2023-05-02 12:12:39 +1200

    Add poret

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,7 @@ plugins.push(Inert);
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: 'localhost'
     });
 
